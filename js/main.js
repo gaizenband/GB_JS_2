1e926b8cdb9adef8300a7213aad5ce2be1af4632
gaizenband <gazik211@gmail.com>
2020-09-13 16:09:55 +0300

    1st lesson

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
+const imgPath = '../img/';
+const productsBlock = document.querySelector('.products');
+
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    {id: 1, title: 'Notebook', price: 2000, img: imgPath+'notebook.jpg'},
+    {id: 2, title: 'Mouse', price: 20, img: imgPath+'2nd.jpg'},
+    {id: 3, title: 'Keyboard', price: 200, img: imgPath+'3rd.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, img: imgPath+'4th.jpg'},
 ];
+
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
+const renderProduct = (obj) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <h3>${obj.title}</h3>
+                <img src=${obj.img}>
+                <p>${obj.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    const productsList = list.map(item => renderProduct(item));
+
+    productsList.forEach(element => {
+        productsBlock.innerHTML += element;
+    });
+
 };
 
 renderPage(products);
